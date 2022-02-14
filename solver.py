1a27a114c691187b6cb51e449aaca588e98f9066
Danny Pires <dannypires99@gmail.com>
2022-02-14 13:42:17 -0500

    minor edits, no code changes

--- solver.py
+++ solver.py
@@ -7,14 +7,14 @@ def main():
         debug = True;
     with open('words.txt') as f:
         ALL_WORDS = {w.strip() : True for w in f.readlines()};
-    repeat = 'y'
+    repeat = 'y';
     while (repeat != 'n'):
-        print('You have {:d} guesses remaining'.format(6 - guesses))
-        user_word = input("Enter your 5 letter word: ").strip()
+        print('You have {:d} guesses remaining'.format(6 - guesses));
+        user_word = input("Enter your 5 letter word: ").strip();
         correctness = input("Enter _ for not in word, ! for correct spot and ? if in wrong position: ").strip();
         if (correctness == "!!!!!"):
-            print("The word is:", user_word + "!")
-            exit()
+            print("The word is:", user_word + "!");
+            exit(0);
         for i in range(5):
             if debug:
                 print("Looking at letter:", user_word[i])
@@ -35,15 +35,15 @@ def main():
                         continue
                     elif correctness[i] == '?' and (user_word[i] not in word or user_word[i] == word[i]):
                         if debug:
-                            print("Removing:", word, "missing correct letter or in wrong postition:", user_word[i])
+                            print("Removing:", word, "missing correct letter or in wrong postition:", user_word[i]);
                         ALL_WORDS[word] = False;
                         continue;
-        print("Valid Word(s): ", end="")
+        print("Valid Word(s): ", end="");
         for x in ALL_WORDS.keys():
             if ALL_WORDS[x]:
-                print(x, end=" ")
+                print(x, end=" ");
         print("");
         repeat = input("Guess another word? y/n: ").strip();
-        guesses += 1
-        print("")
+        guesses += 1;
+        print("");
 main();
