d6e59b9dcc2757be13b00d0ffc69e8c38d545f22
dannyp99 <dannypires99@gmail.com>
2022-02-14 22:58:20 -0500

    Added code to handle multiple of the same letter and printing valid answers

--- solver.py
+++ solver.py
@@ -24,7 +24,7 @@ def main():
                         if debug:
                             print("Removing:", word, "invalid letter:", user_word[i])
                         ALL_WORDS[word] = False;
-                        for j in range(5-i):
+                        for j in range(i+1, 5):
                             if correctness[j] == '!' and user_word[i] == user_word[j]:
                                 ALL_WORDS[word] = True;
                         continue;
@@ -38,11 +38,28 @@ def main():
                             print("Removing:", word, "missing correct letter or in wrong postition:", user_word[i]);
                         ALL_WORDS[word] = False;
                         continue;
-        print("Valid Word(s): ", end="");
-        for x in ALL_WORDS.keys():
-            if ALL_WORDS[x]:
+                    elif correctness[i] == '?':
+                        for j in range(i+1, 5):
+                            if correctness[j] == '!' and user_word[i] == user_word[j]:
+                                if user_word[i] not in word[:j]:
+                                    ALL_WORDS[word] = False;
+        counter = 0;
+        valid_words = []
+        for k,v in ALL_WORDS.items():
+            if v:
+                counter += 1;
+                valid_words.append(k);
+        if counter > 1:
+            print("Valid Word(s): ", end="");
+            for x in valid_words:
                 print(x, end=" ");
-        print("");
+            print("");
+        elif counter == 1:
+            print("The answer is:", valid_words[0] + "!");
+            exit(0);
+        else:
+            print("No valid words, somethings wrong!!!");
+            exit(1);
         repeat = input("Guess another word? y/n: ").strip();
         guesses += 1;
         print("");
